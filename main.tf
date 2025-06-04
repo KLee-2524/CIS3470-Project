acd835428a51369bb1fddc9cbe26d7bf779cc7ff
KLee-2524 <klee.337x@gmail.com>
2025-06-03 22:14:00 -0700

    NorCal subnet

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+  subnet_id     = "subnet-053b1215abb0fd1ce"
 
   tags = {
     Name = "HelloWorld"
