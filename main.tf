1119fdfd38f48502e37687d960298c7f8c87487b
KLee-2524 <klee.337x@gmail.com>
2025-06-04 10:52:48 -0700

    CIS3470 naming convention

--- main.tf
+++ main.tf
@@ -14,21 +14,21 @@
 #  owners = ["979382823631"] # Bitnami
 #}
 
-resource "aws_vpc" "test" {
+resource "aws_vpc" "CIS3470-vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "test-vpc"
+    Name = "CIS3470-vpc"
   }
 }
 
-resource "aws_subnet" "test-subnet" {
+resource "aws_subnet" "CIS3470-subnet" {
   vpc_id            = aws_vpc.test.id
   cidr_block        = "172.16.10.0/24"
   availability_zone = "us-west-1a"
 
   tags = {
-    Name = "test-subnet"
+    Name = "CIS3470-subnet"
   }
 }
 
@@ -38,6 +38,6 @@ resource "aws_instance" "web" {
   subnet_id     = aws_subnet.test-subnet.id
 
   tags = {
-    Name = "HelloWorld"
+    Name = "CIS3470-WinSer22"
   }
 }
