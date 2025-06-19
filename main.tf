b235b8012025101a345a86a0554c98a9e0eba93a
KLee-2524 <klee.337x@gmail.com>
2025-06-19 14:51:36 -0700

    ned1313 Terraform Getting Started

--- main.tf
+++ main.tf
@@ -14,21 +14,63 @@
 #  owners = ["979382823631"] # Bitnami
 #}
 
+# NETWORKING #
 resource "aws_vpc" "cis3470-vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block           = "172.16.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "CIS3470-vpc"
   }
 }
 
+resource "aws_internet_gateway" "cis3470-gateway" {
+  vpc_id = aws_vpc.cis3470-vpc.id
+}
+
 resource "aws_subnet" "cis3470-subnet" {
-  vpc_id            = aws_vpc.cis3470-vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-1a"
+  vpc_id                  = aws_vpc.cis3470-vpc.id
+  cidr_block              = 172.16.0.0/24
+  availability_zone       = "us-west-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "CIS3470-subnet"
+  }
+}
+
+# ROUTING #
+resource "aws_route_table" "cis3470-route-table" {
+  vpc_id = aws_vpc.cis3470-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cis3470-gateway.id
+  }
+}
+
+resource "aws_route_table_association" "cis3470-subnet" {
+  subnet_id      = aws_subnet.cis3470-subnet.id
+  route_table_id = aws_route_table.cis3470-route_table.id
+}
+
+# SECURITY GROUP #
+resource "aws_security_group" "cis3470-sg" {
+  name   = cis3470-sg
+  vpc_id = aws_vpc.cis3470-vpc.id
+
+  ingress {
+    from_port  = 80
+    to_port    = 80
+    protocol   = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_block = ["0.0.0.0/0"]
   }
 }
 
@@ -36,6 +78,8 @@ resource "aws_instance" "cis3740-win-ser-22" {
   ami           = "ami-06fe666da1b90024e"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.cis3470-subnet.id
+  
+  vpc_security_group_ids = [aws_security_group.cis3470-sg.id]
 
   tags = {
     Name = "CIS3470-WinSer22"
