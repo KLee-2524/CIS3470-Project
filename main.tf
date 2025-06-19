3c2d6efc964e2ae9dba3704c5e947a1f33b5d91d
KLee-2524 <klee.337x@gmail.com>
2025-06-19 15:11:25 -0700

    cidr_blockS

--- main.tf
+++ main.tf
@@ -63,14 +63,14 @@ resource "aws_security_group" "cis3470-sg" {
     from_port  = 80
     to_port    = 80
     protocol   = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port  = 0
     to_port    = 0
     protocol   = "-1"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
