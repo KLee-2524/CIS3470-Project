5b2eca0ab4da63cf04581d37ee3528cca54b9b29
KLee-2524 <klee.337x@gmail.com>
2025-07-01 15:59:42 -0700

    variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
+
+variable "os_prefix" {
+    description = "Prefix each resource name with this description"
+    type    = string
+    default = "KALI"
+}
