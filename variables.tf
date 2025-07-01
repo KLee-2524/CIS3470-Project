84c057f82e5caa4aa98f874a6f5bc8e3c8781f40
KLee-2524 <klee.337x@gmail.com>
2025-07-01 16:01:42 -0700

    remove variable

--- variables.tf
+++ variables.tf
@@ -2,9 +2,3 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
-
-variable "os_prefix" {
-    description = "Prefix each resource name with this description"
-    type    = string
-    default = "KALI"
-}
