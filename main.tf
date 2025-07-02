3f5e90031b9e43e5da730d0b69dda49628457100
KLee-2524 <klee.337x@gmail.com>
2025-07-01 17:47:50 -0700

    RDP

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ resource "aws_security_group" "kali-sg" {
   }
 
   ingress {
-    from_port  = 80
-    to_port    = 80
+    from_port  = 3389
+    to_port    = 3389
     protocol   = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
